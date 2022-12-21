974e6c851a0489c05e34870960675552262816af
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 23:37:56 +0200

    Update mody.py

--- mody.py
+++ mody.py
@@ -7,7 +7,7 @@ import base64
 #image = Image.open("C:/medo1.jpg")
 #st.image(image,use_column_width=True)
 url="https://github.com/dentist118/year/blob/main/awesome-gif.gif"
-st.image(url)
+st.image([url])
 n_words =st.number_input('Type the number of words you want to generate')
 seed_text =st.text_input('Type the number of words you want to generate after')
 
