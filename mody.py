ba85c7b8311ebeb02c07ea9a432f3e594bce1427
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 23:33:58 +0200

    Update mody.py

--- mody.py
+++ mody.py
@@ -3,21 +3,6 @@ import pathlib
 from PIL import Image
 st.title("DR. MAHMOUD SITE.")
 import base64
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('blue_bg.png')    
 
 #image = Image.open("C:/medo1.jpg")
 #st.image(image,use_column_width=True)
